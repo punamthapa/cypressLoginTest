b9874b138b699cb2bb53580755daa0b7869aaee9
punamthapa <punam.chhetri.372@gmail.com>
2023-08-30 12:03:11 +0545

    test

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+        cy.visit("https://practicetestautomation.com/practice-test-login/")
+     
+    })
+  
+    it('displays two todo items by default', () => {
+     
+    })
+})
