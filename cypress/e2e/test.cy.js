9e7127bd2617b646e0e068ec33e4aa83fd8f2871
punamthapa <punam.chhetri.372@gmail.com>
2023-09-20 19:22:18 +0545

    empty validation test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -8,15 +8,28 @@ describe('example to-do app', () => {
 
     it('empty validation test', () => {
         cy.get("#submit").click();
-        cy.get("#error").should("contain","Your username is invalid!");
-      
-
-       
+        cy.get("#error").should("contain","Your username is invalid!");  
     })
   
-    // it('invalid login test', () => {
-    //     cy.get("#username").;
+    it('invalid password login test', () => {
+        cy.get("#username").type("student");
+        cy.get("#password").type("testt");
+        cy.get("#submit").click();
+        cy.get("#error").should("contain","Your password is invalid!");
+    })
 
-       
-    // })
+    it('invalid username login test', () => {
+        cy.get("#username").type("student1");
+        cy.get("#password").type("Password123");
+        cy.get("#submit").click();
+        cy.get("#error").should("contain","Your username is invalid!");
+    })
+
+    it('valid login test', () => {
+        cy.get("#username").type("student");
+        cy.get("#password").type("Password123");
+        cy.get("#submit").click();
+        cy.get("h1").should("contain","Logged In Successfully");
+        cy.url().should('include','/logged-in-successfully/');
+    })
 })
